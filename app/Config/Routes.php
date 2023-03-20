64568c08a7be1686ec3191807714cd674b73569f
muhammadsyafiqakmal06 <akmal.indo456@gmail.com>
2023-03-20 10:38:50 +0700

    Penambahan Halaman Register

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,7 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/auth', 'Admin::login');
 $routes->post('/register', 'AuthController::register');
+$routes->get('/register', 'Admin::register');
 $routes->post('/login', 'AuthController::login');
 $routes->get('/logout', 'AuthController::logout');
 
--- app/Views/admin/register.php
+++ app/Views/admin/register.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Login Admin | Kelurahan Gilangharjo</title>
+    <title>Register Admin | Kelurahan Gilangharjo</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
@@ -34,12 +34,12 @@
     <div class="limiter">
         <div class="container-login100">
             <div class="wrap-login100">
-                <form action="<?= base_url('/register'); ?>" method="post" id="login" class="login100-form validate-form">
+                <form action="<?= base_url('/login'); ?>" method="post" id="login" class="login100-form validate-form">
                     <div class="logo text-center">
                         <img src="img/Bantul.png" alt="" style="width: 15%; padding:5px;">
                         <img src="img/UAD-warna.png" alt="" style="width: 20%; padding:5px;">
                         <span class="login100-form-title p-b-43" style="color:#b78a02">
-                            Sign in
+                            Register
                         </span>
                     </div>
 
@@ -57,13 +57,17 @@
                         <span class="label-input100">Password</span>
                     </div>
 
-                    <div class="container-login100-form-btn">
-                        <button class="login100-form-btn">
-                            Sign in
-                        </button>
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input class="input100" type="password" name="password_confirm">
+                        <span class="focus-input100"></span>
+                        <span class="label-input100">Konfirmasi Password</span>
                     </div>
 
-                    <a href="<?= base_url('/register'); ?>"> Buat akun disini!</a>
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                            Register
+                        </button>
+                    </div>
                 </form>
 
                 <div class="login100-more" style="background-image: url('img/gilangharjo2.jpg');">
