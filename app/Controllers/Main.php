f40a8c0965d0fee503142c4a0fb7e22e27140f4c
burhanusr <bsultanramadan@gmail.com>
2023-03-20 17:55:22 +0700

    title halaman admin dan user, password_confirm dihilangkan

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -8,71 +8,85 @@ class Main extends BaseController
 
     public function beranda()
     {
-        return view('user/beranda');
+        $data['title'] = 'Beranda';
+        return view('user/beranda', $data);
     }
 
     public function usaha()
     {
-        return view('user/ketUsaha');
+        $data['title'] = 'Surat Keterangan Usaha';
+        return view('user/ketUsaha', $data);
     }
 
     public function domUsaha()
     {
-        return view('user/ketDomUsaha');
+        $data['title'] = 'Surat Keterangan Domisili Usaha';
+        return view('user/ketDomUsaha', $data);
     }
 
     public function sktm()
     {
-        return view('user/ketSKTM');
+        $data['title'] = 'Surat Keterangan Tidak Mampu';
+        return view('user/ketSKTM', $data);
     }
 
     public function penghasilan()
     {
-        return view('user/ketPengahsilan');
+        $data['title'] = 'Surat Keterangan Penghasilan';
+        return view('user/ketPengahsilan', $data);
     }
 
     public function bedaidentitas()
     {
-        return view('user/ketBedaIdentitas');
+        $data['title'] = 'Surat Keterangan Beda Identitas';
+        return view('user/ketBedaIdentitas', $data);
     }
 
     public function ktp()
     {
-        return view('user/ketKtp');
+        $data['title'] = 'Surat Keterangan KTP Dalam Proses';
+        return view('user/ketKtp', $data);
     }
 
     public function kematian()
     {
-        return view('user/ketKematian');
+        $data['title'] = 'Surat Keterangan Kematian';
+        return view('user/ketKematian', $data);
     }
 
     public function kelahiran()
     {
-        return view('user/ketKelahiran');
+        $data['title'] = 'Surat Keterangan Kelahiran';
+        return view('user/ketKelahiran', $data);
     }
 
     public function skck()
     {
-        return view('user/ketSkck');
+        $data['title'] = 'Surat Pengantar SKCK';
+        return view('user/ketSkck', $data);
     }
 
     public function kehilangan()
     {
-        return view('user/ketKehilangan');
+        $data['title'] = 'Surat Pengantar Kehilangan';
+        return view('user/ketKehilangan', $data);
     }
 
     public function domisili()
     {
-        return view('user/ketDomisili');
+        $data['title'] = 'Surat Pernyataan Domisili';
+        return view('user/ketDomisili', $data);
     }
 
     public function keterangan()
     {
-        return view('user/suratKeterangan');
+        $data['title'] = 'Surat Keterangan';
+        return view('user/suratKeterangan', $data);
     }
 
     public function kontak()
     {
-        return view('user/kontak');
+        $data['title'] = 'Kontak';
+        return view('user/kontak', $data);
     }
 }
--- app/Views/layout/admin_layout.php
+++ app/Views/layout/admin_layout.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Halaman Admin</title>
+    <title><?= $title ?></title>
 </head>
 
 <link rel="icon" type="image/png" href="https://gilangharjo.bantulkab.go.id/assets/files/logo/logo-bantul-sid.png" />
